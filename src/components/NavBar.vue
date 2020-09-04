28c8e762c67c2838433d783e7a270eec3682f9cd
yachen <io260188@gmail.com>
2020-09-04 16:57:25 +0800

    feat: add  popup to nav-item

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,20 @@
 <template>
   <nav :class="{ extend: isNavBarOpen }">
     <h1>口罩即時查</h1>
-    <div class="toggler" @click="isNavBarOpen = !isNavBarOpen">
+    <div class="toggler" @click="toggleNavBar">
       <img :src="isNavBarOpen ? closeNavIcon : toggleNavIcon" />
     </div>
     <div class="menu">
-      <div class="menu-item">口罩供給現況</div>
-      <div class="menu-item">口罩怎麼買</div>
+      <div class="menu-item active">口罩供給現況</div>
+      <div
+        class="menu-item"
+        @click="
+          openPopup();
+          toggleNavBar();
+        "
+      >
+        口罩怎麼買
+      </div>
     </div>
   </nav>
 </template>
@@ -19,6 +27,14 @@ export default {
       toggleNavIcon: require("@/assets/images/toggler.png"),
       closeNavIcon: require("@/assets/images/close.png")
     };
+  },
+  methods: {
+    openPopup() {
+      this.$emit("togglePopup");
+    },
+    toggleNavBar() {
+      this.isNavBarOpen = !this.isNavBarOpen;
+    }
   }
 };
 </script>
@@ -31,7 +47,7 @@ nav {
   top: 0;
   padding: 15px;
   background-color: #fff;
-  z-index: 10;
+  z-index: 2000; // in order to over the z-index of Map
 }
 
 nav.extend {
@@ -46,6 +62,9 @@ nav.extend {
     .menu-item {
       flex: 0 0 56px;
     }
+    .menu-item.active {
+      background-color: #485b6f;
+    }
   }
 }
 
@@ -59,7 +78,7 @@ nav.extend {
   left: 0;
   color: #fff;
   background-color: #35495f;
-  transition: 0.5s;
+  transition: 0.2s;
   overflow: hidden;
   z-index: -1;
   &-item {
@@ -67,9 +86,6 @@ nav.extend {
     padding: 15px 0;
     text-align: center;
     cursor: pointer;
-    &:hover {
-      background-color: #485b6f;
-    }
   }
 }
 
